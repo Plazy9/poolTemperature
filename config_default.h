4a938a5c5ac137957eed681c4ea5ca0ca5b72d30
plazy9 <plazy9@gmail.com>
2023-10-16 21:56:24 +0200

    Add config_default.h file

---
+++ config_default.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+const char* wifiSSID = "Your-SSID";
+const char* wifiPassword = "Your-wifi-password";
+const char* mqttServer = "Your-MQTT-server";
+const char* mqttUser = "Your-MQTT-user";
+const char* mqttPassword = "Your-MQTT-password";
+
+
+#endif
